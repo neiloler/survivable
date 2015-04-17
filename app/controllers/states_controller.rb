8a3a95a0086630c81cfab0cd93b4b94c4c999b64
Neil Oler <neiloler@gmail.com>
2015-04-17 01:47:31 -0600

    Sort and slice data.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,9 @@
 class StatesController < ApplicationController
+
+  def index
+
+    survivabilitySortedStates = State.order(population_military: :desc, population_total: :asc, housing_units_total: :asc, housing_units_occupied: :asc, housing_units_vacant: :desc)
+
+    @safestPlaces = survivabilitySortedStates.first(10)
+    @scariestPlaces = survivabilitySortedStates.last(10)
 end
